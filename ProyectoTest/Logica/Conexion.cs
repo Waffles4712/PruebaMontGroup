a728b2b9abb0616e5ff76b697def75f2ec424302
NeilTafur <armstrong4712@gmail.com>
2023-05-07 17:33:50 -0500

    first commit neil

---
+++ ProyectoTest/Logica/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTest.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Server=NEIL\\SQLEXPRESS; DataBase=TIENDA_ONLINE;Integrated Security=true; TrustServerCertificate=True";
+    }
+}
